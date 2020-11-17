1e88cb4c70c2a1e47ef76f164c170ba0080e09f6
kacluk123 <sakuy124@gmail.com>
2020-11-17 21:22:02 +0100

    full-bot-functionality

--- src/bots/botTypes/ad.ts
+++ src/bots/botTypes/ad.ts
@@ -3,18 +3,19 @@ import { botTypes } from '../bots.entity'
 import { BotsService } from '../bots.service'
 // var schedule = require('node-schedule');
 import { scheduleJob, Job } from 'node-schedule'
-import { ADDRCONFIG } from 'dns';
 const Discord = require('discord.js');
 const client = new Discord.Client()
 
 // const client = new Discord.Client();
 export interface IAdBotConfig {
-  ads: {
-    time: string 
-    day: string
-    message: string
-    id: string
-  }[]
+  ads: ISingleAd[]
+}
+
+export interface ISingleAd {
+  time: string 
+  day: string
+  message: string
+  id: string
 }
 
 export interface MainOptions {
@@ -34,21 +35,50 @@ export class AdBot implements MainBot {
     this.ads = usabilityConfig.ads
     this.mainOptions = mainOptions
     setInterval(() => {
-      this.getBotData()
-    }, 5000)
+      this.reloadBot()
+    }, 15000)
   }
 
   public restart = async () => {
     this.run()
   }
 
-  public async getBotData() {
+  public async reloadBot() {
     const { config, ...rest } = await this.botsService.getBot(this.mainOptions.id)
+    
+    this.makeActionDependsOnCurrentBotState(rest, config)
 
     this.mainOptions = rest
+    
     this.ads = config.ads
+  }
 
-    this.rebuildAds(config.ads)
+  private makeActionDependsOnCurrentBotState(mainOptions: MainOptions , config: IAdBotConfig) {
+    if (this.allSchedules.size === 0 && mainOptions.isActive) {
+      this.allSchedules = this.runAds(config.ads)
+    }
+
+    if (!mainOptions.isActive) {
+      this.turnOffAllBots()
+    }
+
+    if (this.allSchedules.size > 0 && this.mainOptions.isActive) {
+      this.rebuildAds(config.ads)
+    }
+  }
+
+  private turnOffAllBots() {
+    this.allSchedules.forEach((value,key,map) => {
+      this.allSchedules.get(key).cancel()
+    })
+    this.allSchedules = new Map()
+  }
+
+  private runAd = (ad: ISingleAd, ads: Map<string, Job>) => {
+    const [hour, minute, second] = ad.time.split(':')
+      ads.set(ad.id, scheduleJob({hour: hour, minute: minute, dayOfWeek: ad.day, second: second}, () => {
+        client.channels.cache.get('736938961137827850').send(ad.message)
+    }))
   }
 
   public rebuildAds = (ads: IAdBotConfig['ads']) => {
@@ -56,38 +86,29 @@ export class AdBot implements MainBot {
       const existedAd = this.ads.find(existedAd => existedAd.id === ad.id)
       const existedSchedule = this.allSchedules.get(ad.id)
 
-      // if (existedAd && (JSON.stringify(existedAd) !== JSON.stringify(ad))) {
-      //   existedSchedule.cancel()
-      //   const [hour, minute, second] = ad.time.split(':')
-      //   this.allSchedules.set(ad.id, scheduleJob({hour: hour, minute: minute, dayOfWeek: ad.day, second: second}, () => {
-      //     if (this.mainOptions.isActive) {
-      //       client.channels.cache.get('736938961137827850').send(ad.message)
-      //     }
-      //   }))
-      // } 
+      if (existedAd && (JSON.stringify(existedAd) !== JSON.stringify(ad))) {
+        existedSchedule.cancel()
+        this.runAd(ad, this.allSchedules)
+      } 
       if (!existedSchedule) {
         console.log('yes')
-        const [hour, minute, second] = ad.time.split(':')
-        this.allSchedules.set(ad.id, scheduleJob({hour: hour, minute: minute, dayOfWeek: ad.day, second: second}, () => {
-          if (this.mainOptions.isActive) {
-            client.channels.cache.get('736938961137827850').send(ad.message)
-          }
-        }))
+        this.runAd(ad, this.allSchedules)
       }
     })
   }
   
+  private runAds = (ads: IAdBotConfig['ads']) => {
+    return ads.reduce((prev,curr) => {
+      this.runAd(curr, prev)
+      return prev
+    }, new Map<string, Job>())
+  }
 
   public run() {
     client.on('ready', () => {
-      this.ads.forEach(ad => {
-        const [hour, minute, second] = ad.time.split(':')
-        this.allSchedules.set(ad.id, scheduleJob({hour: hour, minute: minute, dayOfWeek: ad.day, second: second}, () => {
-          if (this.mainOptions.isActive) {
-            client.channels.cache.get('736938961137827850').send(ad.message)
-          }
-        }))
-      })
+      if (this.mainOptions.isActive) {
+        this.allSchedules = this.runAds(this.ads)
+      }
     })
 
     client.login(this.mainOptions.token);
