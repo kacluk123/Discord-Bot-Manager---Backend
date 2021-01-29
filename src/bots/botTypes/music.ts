f2a208e4b1348caed547e696ace1d1109b50211c
kacluk123 <sakuy124@gmail.com>
2021-01-29 22:52:23 +0100

    playlist

--- src/bots/botTypes/music.ts
+++ src/bots/botTypes/music.ts
@@ -3,19 +3,34 @@ const Discord = require('discord.js');
 import { Client, TextChannel, VoiceChannel, Message, Channel } from 'discord.js'
 import { BotsService } from '../bots.service'
 import { MainOptions } from './ad' 
+import { all } from 'ramda';
 const ytdl = require('ytdl-core')
 
 export interface IMusicBotConfig {
   playlist: string[]
 }
 
+function* playList(arr: string[]) {
+  var index = 0;
+  while(true) {
+    yield arr[index]
+    if (index + 1 > arr.length) {
+      index = 0
+    } else {
+      index++
+    }
+  }
+}
+
+const x = playList(['https://www.youtube.com/watch?v=j5dFe-WKuPs', 'https://www.youtube.com/watch?v=d4_6N-k5VS4'])
+
 export class MusicBot implements MainBot {
-  private playlist: string[]
+  private playlist: string[] = ['https://www.youtube.com/watch?v=j5dFe-WKuPs', 'https://www.youtube.com/watch?v=d4_6N-k5VS4']
   client: Client = new Client();
   mainOptions: MainOptions
 
   constructor(private readonly botsService: BotsService, usabilityConfig: IMusicBotConfig, mainOptions: MainOptions) {
-    this.playlist = usabilityConfig.playlist
+    // this.playlist = usabilityConfig.playlist
     this.mainOptions = mainOptions
   }
 
@@ -38,8 +53,12 @@ export class MusicBot implements MainBot {
     try {
       const streamOptions = { seek: 0, volume: 1 };
       const connection = await channel.join()
-      const stream = ytdl('https://www.youtube.com/watch?v=-I9QH6gI6wM', { filter : 'audioonly' });
-      connection.play(stream, streamOptions);
+      const stream = ytdl('https://www.youtube.com/watch?v=j5dFe-WKuPs', { filter : 'audioonly' })
+      connection.play(stream, streamOptions).on('finish', () => {
+        const stream2 = ytdl('https://www.youtube.com/watch?v=d4_6N-k5VS4', { filter : 'audioonly' })
+        connection.play(stream2, streamOptions)
+      })
+      
     } catch(err) {
       console.log(err)
     }
