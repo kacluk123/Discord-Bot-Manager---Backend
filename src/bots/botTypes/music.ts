8d73aa437c70ede7e2a5741a376ae62095ab3466
kacluk123 <sakuy124@gmail.com>
2021-02-07 23:14:29 +0100

    music-bot

--- src/bots/botTypes/music.ts
+++ src/bots/botTypes/music.ts
@@ -1,36 +1,25 @@
 import { MainBot } from '../bots.factory'
 const Discord = require('discord.js');
-import { Client, TextChannel, VoiceChannel, Message, Channel } from 'discord.js'
+import { Client, TextChannel, VoiceChannel, Message, Channel, VoiceConnection, StreamDispatcher } from 'discord.js'
 import { BotsService } from '../bots.service'
 import { MainOptions } from './ad' 
-import { all } from 'ramda';
 const ytdl = require('ytdl-core')
 
 export interface IMusicBotConfig {
   playlist: string[]
 }
 
-function* playList(arr: string[]) {
-  var index = 0;
-  while(true) {
-    yield arr[index]
-    if (index + 1 > arr.length) {
-      index = 0
-    } else {
-      index++
-    }
-  }
-}
-
-const x = playList(['https://www.youtube.com/watch?v=j5dFe-WKuPs', 'https://www.youtube.com/watch?v=d4_6N-k5VS4'])
-
 export class MusicBot implements MainBot {
-  private playlist: string[] = ['https://www.youtube.com/watch?v=j5dFe-WKuPs', 'https://www.youtube.com/watch?v=d4_6N-k5VS4']
+  private playlist: PlayList
   client: Client = new Client();
   mainOptions: MainOptions
+  musicStream: StreamDispatcher
 
   constructor(private readonly botsService: BotsService, usabilityConfig: IMusicBotConfig, mainOptions: MainOptions) {
-    // this.playlist = usabilityConfig.playlist
+    this.playlist = new PlayList([
+      'https://www.youtube.com/watch?v=d4_6N-k5VS4',
+      'https://www.youtube.com/watch?v=qMxX-QOV9tI'
+    ])
     this.mainOptions = mainOptions
   }
 
@@ -51,17 +40,24 @@ export class MusicBot implements MainBot {
 
   async play(channel: VoiceChannel) {
     try {
-      const streamOptions = { seek: 0, volume: 1 };
       const connection = await channel.join()
-      const stream = ytdl('https://www.youtube.com/watch?v=j5dFe-WKuPs', { filter : 'audioonly' })
-      connection.play(stream, streamOptions).on('finish', () => {
-        const stream2 = ytdl('https://www.youtube.com/watch?v=d4_6N-k5VS4', { filter : 'audioonly' })
-        connection.play(stream2, streamOptions)
-      })
+
+      this.musicQuee(connection)
       
     } catch(err) {
       console.log(err)
     }
+  }
+
+  async musicQuee(connection: VoiceConnection) {
+    const songUrl = this.playlist.song
+    const stream = ytdl(songUrl, { filter : 'audioonly' })
+    
+    this.musicStream = connection.play(stream,  { seek: 0, volume: 1 }).on('finish', () => {
+      this.playlist.nextSong()
+      this.musicQuee(connection)
+    })
+    
   }
 
   private getCommands(message: Message) {
@@ -73,15 +69,54 @@ export class MusicBot implements MainBot {
             await message.channel.send('Now bot will be playing music on this channel!')
             await this.play(channel)
           }
-          // this.botsService.editBot({
-          //   config: {
-          //     ...bot.config,
-          //     channelsToSend: Array.from(this.channelsWithAds)
-          //   }
-          // }, bot)
+        },
+      ],
+      [
+        '!pause', async () => {
+          const channel = message.member.voice.channel
+          if (((logChannel): logChannel is VoiceChannel => logChannel.type === 'voice')(channel)) {
+            this.musicStream.pause()
+          } 
         }
       ],
+      [
+        '!start', async () => {
+          const channel = message.member.voice.channel
+          if (((logChannel): logChannel is VoiceChannel => logChannel.type === 'voice')(channel)) {
+            this.musicStream.resume()
+          } 
+        }
+      ],
+      [
+        '!skip', async () => {
+          const channel = message.member.voice.channel
+          if (((logChannel): logChannel is VoiceChannel => logChannel.type === 'voice')(channel)) {
+            this.musicStream.destroy()
+            this.playlist.nextSong()
+            await this.play(channel)
+          } 
+        }
+      ]
     ])
   }
 }
 
+class PlayList {
+  public currentIndex: number = 0
+  
+  constructor(private songs: string[]) {}
+
+  public nextSong() {
+    if (this.currentIndex + 1 > this.songs.length - 1) {
+      this.currentIndex = 0
+    } else {
+      this.currentIndex++
+    }
+  }
+
+  get song() {
+    return this.songs[this.currentIndex]
+  }
+
+
+}
